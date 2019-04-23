5bf260caccf5b8244eeeefb29782fe075a7fabd5
Moonkaman <nickbatmanglidj@gmail.com>
2019-04-23 13:51:50 -0600

    Complete file_io.py

--- src/file_io.py
+++ src/file_io.py
@@ -7,12 +7,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
+with open('foo.txt', 'r') as f:
+    print(f.read())
+    f.close()
 
 # YOUR CODE HERE
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    for i in range(1000):
+        f.write('I will not use semicolons. \n')
+    f.close()
